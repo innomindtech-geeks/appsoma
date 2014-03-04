b79f50ceacbd774c1736c711dccfec54ff53b094
Dan Winkler <dan@danwink.com>
2014-03-04 13:03:11 -0600

    fixed login button

--- wp-content/themes/Appsoma/nav.php
+++ wp-content/themes/Appsoma/nav.php
@@ -15,7 +15,7 @@
         <?php }
 		else { ?>
                   
-			 <a href="https://appsoma.com/login"  class="login" data-toggle="modal" data-target="#login">Login</a>
+			 <a href="https://appsoma.com/login"  class="login" <?php //data-toggle="modal" data-target="#login"?>>Login</a>
              
               <?php 
 		}
